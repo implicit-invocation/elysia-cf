dba61eb8cdcad28242b568f6bb452f6a18b27202
implicit-invocation <tao@dongbat.com>
2023-10-16 11:39:26 +0700

    update template

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import cors from "@elysiajs/cors";
 import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
+import cors from "./patch/cors";
 import { cfEnv, handleWorker } from "./plugins/cf";
 import { Env } from "./type";
 
